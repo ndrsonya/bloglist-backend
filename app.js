636623314f527743a5e811b3f7bd59ec30ce2c8b
ndrsonya <altusar.studies@gmail.com>
2021-03-24 17:04:14 +0200

    4.18 adde

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 
 logger.info('connecting to', url)
 
@@ -26,6 +28,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
